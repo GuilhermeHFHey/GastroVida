01799cb56bf6ae678407ca373e615143e2a8fca4
GuilhermeHey <guilhermehfhey@gmail.com>
2021-07-04 19:01:40 -0300

    Aula 04, create and read

--- app/views.py
+++ app/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from app.forms import PacientesForm
+from app.models import Pacientes
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    data = {}
+    data['db'] = Pacientes.objects.all()
+    return render(request, 'index.html', data)
 
 def form(request):
     data = {}
     data['form'] = PacientesForm()
     return render(request, 'form.html', data)
+
+def create(request):
+    form = PacientesForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('home')
+    
+def view(request, pk):
+    data = {}
+    data['db'] = Pacientes.objects.get(pk=pk)
+    return render(request, 'view.html', data)
