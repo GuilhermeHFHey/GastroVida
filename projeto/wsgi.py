fdbeba5c9974bba5cd68b0a0c704f0301524b25b
Bruno Alves <bruno.barbosa2707@gmail.com>
2021-11-16 17:55:38 -0300

    Deploy da aplicação

--- projeto/wsgi.py
+++ projeto/wsgi.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
 import os
 from whitenoise import WhiteNoise
 from django.core.wsgi import get_wsgi_application
-from projeto import MyWSGIApp
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'projeto.settings')
 
