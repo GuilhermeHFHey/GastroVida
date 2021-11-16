88811d784187a3f4631964e0402e44c10f574794
Bruno Alves <bruno.barbosa2707@gmail.com>
2021-11-16 19:44:27 -0300

    Deploy da aplicação

--- projeto/settings.py
+++ projeto/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-iba%%5t@5hx*qlk&h9=duql)x-c_z_x8!xnjwvhred*95jq0!5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
